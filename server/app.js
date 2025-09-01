9bfc9b00b728b60a16f2585e67eda0bf168de57a
Connor <jenkinsc30@cardiff.ac.uk>
2025-09-01 20:03:42 +0100

    Added fetching images by month, manual testing shows this works. Added authentication to all routes

--- server/app.js
+++ server/app.js
@@ -1,33 +1,25 @@
 const express = require('express')
 const {scanDirectory} = require("./services/directoryScanner");
-const {getAllImages, getLatestImages} = require("./repository/repoService");
+const {getAllImages, getLatestImages, getAllImagesFromMonth} = require("./repository/repoService");
 const {imageToBase64} = require("./services/imageToBase64")
 const app = express()
 const port = 3000
 
 require('dotenv').config();
 
-app.get('/api/scan', (req, res) => {
+app.get('/api/:user-:pass/scan', (req, res) => {
+    const { user, pass } = req.params;
+    if(user !== process.env.UNAME || pass !== process.env.PASSWORD) {
+        return res.status(401).json({ error: 'Authentication failed' });
+    }
     // scan the directory using directoryScanner, this will be called on app launch
     scanDirectory().then(r =>
     console.log(`Scanned directory: ${r.length} images found`));
-    res.json({ message: 'Hello from the server!' })
-})
-
-app.get('/api/images', (req, res) => {
-    // run directoryScanner and return array of images
-    const directoryScanner = require('./services/directoryScanner')
-    directoryScanner.scanDirectory()
-        .then(images => {
-            res.json(images)
-        })
-        .catch(err => {
-            console.error('Error scanning directory:', err)
-            res.status(500).json({ error: 'Failed to scan directory' })
-        })
+    res.status(200).json({ message: 'Ready' })
 })
 
 app.get('/api/:username-:pass', (req, res) => {
+    // URI: /api/'user'-'password'
      const { username, pass } = req.params;
      // Here you would typically validate the username and password
      // For demonstration, we'll just return them
@@ -40,7 +32,13 @@ app.get('/api/:username-:pass', (req, res) => {
 
 
 // get all images
-app.get('/api/images/all', (req, res) => {
+app.get('/api/:user-:pass/images/all', (req, res) => {
+    // Auth
+    const { user, pass } = req.params;
+    if(user !== process.env.UNAME || pass !== process.env.PASSWORD) {
+        return res.status(401).json({ error: 'Authentication failed' });
+    }
+
     getAllImages()
         .then(imageDetails => {
             // convert imageDetails to base64
@@ -53,7 +51,12 @@ app.get('/api/images/all', (req, res) => {
         })
 })
 
-app.get('/api/images/latest', (req, res) => {
+app.get('/api/:user-:pass/images/latest', (req, res) => {
+    // Auth
+    const { user, pass } = req.params;
+    if(user !== process.env.UNAME || pass !== process.env.PASSWORD) {
+        return res.status(401).json({ error: 'Authentication failed' });
+    }
     getLatestImages()
         .then(imageDetails => {
             // convert imageDetails to base64
@@ -63,6 +66,29 @@ app.get('/api/images/latest', (req, res) => {
         .catch(err => {
             console.error('Error fetching latest images:', err);
             res.status(500).json({ error: 'Failed to fetch latest images' })
+        })
+})
+
+app.get('/api/:user-:pass/images/:month-:year', (req, res) => {
+    // Auth
+    const { user, pass } = req.params;
+    if(user !== process.env.UNAME || pass !== process.env.PASSWORD) {
+        return res.status(401).json({ error: 'Authentication failed' });
+    }
+
+    const { month, year } = req.params;
+    if (!month || !year) {
+        return res.status(400).json({ error: 'Month and year are required' });
+    }
+    getAllImagesFromMonth(month, year)
+        .then(imageDetails => {
+            // convert imageDetails to base64
+            const imagesWithBase64 = imageToBase64(imageDetails)
+            res.status(200).json(imagesWithBase64)
+        })
+        .catch(err => {
+            console.error('Error fetching images for month:', err);
+            res.status(500).json({ error: 'Failed to fetch images for month'+month+'/'+year })
         })
 })
 
