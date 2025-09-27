7d893eb21134c80c1634e950879d8351e60603c3
Connor <jenkinsc30@cardiff.ac.uk>
2025-09-27 22:46:54 +0100

    basic xml styling for main activity working

--- app/app/src/main/java/com/example/mycloud/MainActivity.kt
+++ app/app/src/main/java/com/example/mycloud/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mycloud
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,32 +16,7 @@ import com.example.mycloud.ui.theme.MyCloudTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyCloudTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyCloudTheme {
-        Greeting("Android")
+        Log.d("MainActivity", "onCreate called")
+        setContentView(R.layout.activity_main)
     }
 }
